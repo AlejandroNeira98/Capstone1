acf3373c9b432aed9c825d9d5de3bfd1900b2a17
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-02-01 01:03:02 -0500

    Add JavaScript Object & Styling Changes

---
+++ speakers.js
@@ -0,0 +1,42 @@
+const speakers = [
+  {
+  'profile_image': '',
+  'name':'',
+  'degree': '',
+  'presentation':''
+  },
+  {
+  'profile_image': '',
+  'name':'',
+  'degree': '',
+  'presentation':''
+  },
+  {
+  'profile_image': '',
+  'name':'',
+  'degree': '',
+  'presentation':''
+  },
+  {
+  'profile_image': '',
+  'name':'',
+  'degree': '',
+  'presentation':''
+  },
+  {
+  'profile_image': '',
+  'name':'',
+  'degree': '',
+  'presentation':''
+  },
+  {
+  'profile_image': '',
+  'name':'',
+  'degree': '',
+  'presentation':''
+  },
+]
+
+speakers.forEach(element => {
+  document.createElement(div)
+});
