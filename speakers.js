03000af48ef3347f7248c19637c2140bd66a2e86
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-02-01 01:48:43 -0500

    Add dinamically generated HTML

--- speakers.js
+++ speakers.js
@@ -36,7 +36,26 @@ const speakers = [
   'presentation':''
   },
 ]
+ 
+let text= "";
 
 speakers.forEach(element => {
-  document.createElement(div)
+  
+  text = `
+  
+    <div class="profile-img">
+      <img src="${element.profile_image}" alt="Speaker">
+    </div>
+    <div class="speaker-presentation">
+      <h3 class="name">${element.name}</h3>
+      <h4 class="degree">${element.degree}</h4>
+      <p class="presentation">${element.presentation}</p>
+    </div>
+   
+  `;
+  let speaker = document.createElement(div);
+  speaker.classList.add('speaker');
+  speaker.innerHTML = text;
+
+  document.getElementById('featured-speakers').appendChild(speaker);
 });
